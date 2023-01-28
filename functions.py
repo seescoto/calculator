0ff9447702fe106563c9e0fec4d114ea94326653
Sofia <seescoto2000@gmail.com>
2023-01-28 16:51:18 -0500

    started postfix function

--- functions.py
+++ functions.py
@@ -2,15 +2,18 @@
 
 ###main entry point 
 fieldText = ""
+fieldDisplay =""
 base = 10
+basePrefix =""
 
 def addEquationEnd(field, equation):  
-   global fieldText 
-   fieldText += str(equation)
+   global fieldText, fieldDisplay 
+   fieldDisplay += str(equation)
+   #if neg number, 
    #delete prev content from field 
    field.delete("1.0", "end") #delete from start to end
    #put in new fieldText
-   field.insert("1.0", fieldText) 
+   field.insert("1.0", fieldDisplay) 
 
 def addEquationFront(field, equation):
    #used for negating an entire answer, etc. 
@@ -19,11 +22,23 @@ def addEquationFront(field, equation):
    if len(fieldText) != 0:
       fieldText = str(equation) + "(" + fieldText + ")" 
       #replace content  
+      field.delete("1.0", "end")
       field.insert("1.0", fieldText)
 
-#press equal button
-def calculate(field):
+def setBase(newBase):
+   global base, basePrefix
+   base = newBase 
+   if base == 16:
+      basePrefix = "0X"
+   elif base == 2:
+      basePrefix = "0b"
+
+def equals(field):
    global fieldText 
+   postText = toPostFix(fieldText)
+
+
+
    result = str(eval(fieldText)) 
    #replace equation text 
    field.delete("1.0", "end") 
@@ -35,10 +50,45 @@ def clear(field):
    fieldText = "" 
    field.delete("1.0", "end")
 
-def setBase(newBase):
-   global base 
-   base = newBase 
+def toPostFix(text):
+   postText = ""
+   postStack = []
 
+   #go left to right for input string
+   for char in text:
+      #if an operand (nums plus pi and e), add to postText straight away
+      if char in "0123456789ABCDEF\u03C0e":
+         postText += char + " "
+      #if an operator (l for log, sqrt, n for negative)
+      elif char in "^/*-+l\u221An":
+         checkPriority(char, postStack)
+      #if left parentheses, add to stack no matter what
+      elif char == "(":
+         postStack.append(char) 
+      #else must be right parentheses
+      else:
+         #just add everything else from stack to expression
+         while postStack and top(postStack) != "(":
+            postText += postStack.pop() + " "
+         #now must be left parentheses, if not raise error, if it is pop (
+         if top(postStack) != "(":
+            raise SyntaxError
+         postStack.pop()
+   #now just add rest of stack to postText
+   while postStack:
+      postText += postStack.pop() + " "
+
+   return postText
+            
+
+
+def checkPriority(char, postStack):
+   #check the priority of the operator vs the top of the stack
+   pass 
+
+
+def top(list):
+   return list[-1]
 
 def copyAns(field):
    pass
--- gui.py
+++ gui.py
@@ -109,7 +109,7 @@ buttonSub.grid(row = 4, column = 6, padx = padwidth, pady = padheight)
 buttonAdd = tk.Button(window, text = "+", command = lambda: addEquationEnd(field, "+"), width = buttonwidth)
 buttonAdd.grid(row = 5, column = 6, padx = padwidth, pady = padheight)
 
-buttonEqual = tk.Button(window, text = "=", command = lambda: calculate(field), width = buttonwidth)
+buttonEqual = tk.Button(window, text = "=", command = lambda: equals(field), width = buttonwidth)
 buttonEqual.grid(row = 6, column = 6, padx = padwidth, pady = padheight)
 
 
@@ -136,25 +136,24 @@ buttonPower.grid(row = 6, column = 2, padx = padwidth, pady = padheight)
 
 #col3 buttons 
 #help, copy ans, pi, e, log, sqrt
-buttonHelp = tk.Button(window, text = "? HELP ?", command = lambda: openHelpPage(), width = buttonwidth)
+buttonHelp = tk.Button(window, text = "Help!", command = lambda: openHelpPage(), width = buttonwidth)
 buttonHelp.grid(row = 1, column = 1, padx = padwidth, pady = padheight)
 
-buttonAns = tk.Button(window, text = "ANS", command = lambda: copyPrevResult(field), width = buttonwidth)
+buttonAns = tk.Button(window, text = "ans", command = lambda: copyAns(field), width = buttonwidth)
 buttonAns.grid(row = 2, column = 1, padx = padwidth, pady = padheight)
 
 buttonPi = tk.Button(window, text = "\u03C0", command = lambda: addEquationEnd(field, "\u03C0"), width = buttonwidth)
 buttonPi.grid(row = 3, column = 1, padx = padwidth, pady = padheight)
 
-buttonPar1 = tk.Button(window, text = "(", command = lambda: addEquationEnd(field, "("), width = buttonwidth)
+buttonPar1 = tk.Button(window, text = "e", command = lambda: addEquationEnd(field, "e"), width = buttonwidth)
 buttonPar1.grid(row = 4, column = 1, padx = padwidth, pady = padheight)
 
-buttonPar2 = tk.Button(window, text = ")", command = lambda: addEquationEnd(field, ")"), width = buttonwidth)
+buttonPar2 = tk.Button(window, text = "log", command = lambda: addEquationEnd(field, "log("), width = buttonwidth)
 buttonPar2.grid(row = 5, column = 1, padx = padwidth, pady = padheight)
 
-buttonPower = tk.Button(window, text = "^", command = lambda: addEquationEnd(field, "^"), width = buttonwidth)
+buttonPower = tk.Button(window, text = "\u221A", command = lambda: addEquationEnd(field, "\u221A"), width = buttonwidth)
 buttonPower.grid(row = 6, column = 1, padx = padwidth, pady = padheight)
 
-#\u221A1 <- sqrt symbol
 
 #always show window 
 window.mainloop()
