8ed3cc1b4b771ca3b610495eb2bd42e789f2ce46
Sofia <seescoto2000@gmail.com>
2023-02-20 17:42:00 -0500

    add microservice by partner

---
+++ CalculatorMicroservice/MicroserviceClientExample.py
@@ -0,0 +1,35 @@
+#by audrie nielson from https://github.com/audrie-nielsen/CalculatorMicroservice
+
+#   Connects REQ socket to tcp://localhost:5555
+import zmq
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+# ZeroMQ uses bytes to communicate data.
+# When sending a request to the server, encode the value as byte in 1 of 2 ways:
+
+# Option 1: socket.send('[request]'.encode('ASCII'))
+socket.send('16/4'.encode('ASCII'))
+socket.recv()
+
+# Option 2: socket.send((b"[request]")
+socket.send(b"4.0")
+socket.recv()
+
+socket.send(b"3 + 6 / 2 - 9")
+socket.recv()
+
+socket.send('3'.encode('ASCII'))
+socket.recv()
+
+socket.send(b"POP")  # Send request "POP" to retrieve the last equation and result pushed
+res = socket.recv_json() # Use socket.recv_json() to receive the equation and result as a JSON
+print(res)
+
+socket.send('POP'.encode('ASCII'))
+res = socket.recv_json()
+print(res)
---
+++ CalculatorMicroservice/MicroserviceServer.py
@@ -0,0 +1,37 @@
+#by audrie nielson from https://github.com/audrie-nielsen/CalculatorMicroservice
+
+import time
+import zmq
+import json
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+arr = []
+
+while True:
+    #  Waits for next request from client
+    req = socket.recv()
+    print("Received request: %s" % req)
+    time.sleep(1)
+
+    #  If request is "POP", server sends back a JSON containing the last pushed equation and result
+    if req == b"POP":
+        if arr == []:
+            resJSON = {"equation": "empty", "result": "empty"}
+            res = json.dumps(resJSON)
+            socket.send_json(res)
+        else:
+            result = arr[-1].decode("ASCII")
+            arr.pop()
+            equation = arr[-1].decode("ASCII")
+            arr.pop()
+
+            resJSON = {"equation": equation, "result": result}
+            res = json.dumps(resJSON)
+            socket.send_json(res)
+    # Else, adds the request data to the array
+    else:
+        arr.append(req)
+        socket.send(b"Result added to the stack")
