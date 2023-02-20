2c8c391dd8ab443dc71a8f7e5b309d6f3accbd50
Sofia <seescoto2000@gmail.com>
2023-02-20 17:59:44 -0500

    working microservice

--- CalculatorMicroservice/MicroserviceClientExample.py
+++ CalculatorMicroservice/MicroserviceClientExample.py
@@ -3,6 +3,7 @@
 #   Connects REQ socket to tcp://localhost:5555
 import zmq
 context = zmq.Context()
+import json  #translate json to python dictionary 
 
 #  Socket to talk to server
 print("Connecting to server…")
@@ -11,7 +12,7 @@ socket.connect("tcp://localhost:5555")
 
 # ZeroMQ uses bytes to communicate data.
 # When sending a request to the server, encode the value as byte in 1 of 2 ways:
-
+'''
 # Option 1: socket.send('[request]'.encode('ASCII'))
 socket.send('16/4'.encode('ASCII'))
 socket.recv()
@@ -33,3 +34,16 @@ print(res)
 socket.send('POP'.encode('ASCII'))
 res = socket.recv_json()
 print(res)
+'''
+
+#send equation and answer 
+socket.send(b'15/2')
+socket.recv()
+socket.send(b'7.5') 
+socket.recv()
+
+#get last eqiation and answer 
+socket.send(b'POP') 
+res = (socket.recv_json())
+eqDict = json.loads(res)
+print(eqDict['equation'], " = ", eqDict['result'])
